25f7fcc1e396346993b437f6898bb891cf1e8ef3
Ramachandran Narayanan <ramachandran.narayanan@live.com>
2017-10-26 11:17:46 +0530

    Initial Commit

---
+++ MarsRover/Plateau.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover
+{
+    public class Plateau
+    {
+        public int upperXValue { get; set; }
+        public int upperYValue { get; set; }
+
+        int lowerXValue = 0;
+        int lowerYValue = 0;
+
+        public Plateau(int x, int y)
+        {
+            upperXValue = x;
+            upperYValue = y;
+        }
+
+        //This checkd
+        public bool CheckValueWithInRange
+        {
+            get
+            {
+                if (upperXValue >= 0 && upperYValue >= 0 && lowerXValue >= 0 && lowerYValue >= 0)
+                    return true;
+                return false;
+            }
+        }
+    }
+
+    public enum Directions
+    {
+        North,
+        South,
+        East,
+        West
+    }
+}
---
+++ MarsRover/Rover.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover
+{
+    public class Rover
+    {
+        int currentXValue, currentYValue, currentDirection = 0;
+        public Rover(int currentx, int currenty, int direction)
+        {
+            currentXValue = currentx;
+            currentYValue = currenty;
+            currentDirection = direction;
+        }
+
+        public void TurnLeft()
+        {
+
+        }
+
+        public void TurnRight()
+        {
+
+        }
+
+        public void Move()
+        {
+
+        }
+
+    }
+}
